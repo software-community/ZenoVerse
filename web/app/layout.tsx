dbeed043523e3adc7ec38adce6d6ab9e95ddb52d
Shaurya <promispriyam04@gmail.com>
2025-07-05 18:39:48 +0530

    Guide for Frontend layout,page.tsx,components

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,9 @@
+import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Navbar />
+          <main className="min-h-screen">
+            {children}
+          </main>
+        <Footer />
       </body>
     </html>
   );
---
+++ web/components/Navbar.jsx
@@ -0,0 +1,12 @@
+// "use client";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full px-6 py-4 border-b">
+      <h1 className="text-xl font-semibold">ZenoVerse</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
