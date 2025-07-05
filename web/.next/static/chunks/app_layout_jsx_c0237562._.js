9d1ce424fe3c9079b3433db20105e82e42ad3b1b
Kapil-Mulay-1421 <krmulay@gmail.com>
2025-07-05 23:46:09 +0530

    delete .next

--- web/.next/static/chunks/app_layout_jsx_c0237562._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__288fc8c2._.css",
-  "static/chunks/_85b92aac._.js"
-],
-    source: "dynamic"
-});
--- web/.next/static/chunks/app_page_jsx_b6251668._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_8a599d3e._.js"
-],
-    source: "dynamic"
-});
