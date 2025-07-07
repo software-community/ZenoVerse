2a2a45451ec878b9dd4cb36a74f2ae6c7ca29ace
Varun <varunbha012@gmail.com>
2025-07-07 18:22:35 +0530

    Added functions in contracts

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -4,11 +4,24 @@ pragma solidity ^0.8.28;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
+
 contract Lock {
     uint public unlockTime;
     address payable public owner;
 
     event Withdrawal(uint amount, uint when);
+
+    // NFT logic
+    uint256 private _tokenIdCounter;
+
+    // Event for frontend notification
+    event ObservationMinted(address indexed to, uint256 indexed tokenId, string tokenURI);
+
+    // Mapping from tokenId to tokenURI
+    mapping(uint256 => string) private _tokenURIs;
+
+    // Mapping from owner to list of owned token IDs
+    mapping(address => uint256[]) private _ownedTokens;
 
     constructor(uint _unlockTime) payable {
         require(
@@ -31,4 +44,17 @@ contract Lock {
 
         owner.transfer(address(this).balance);
     }
+
+    // mintObservation() restricted to backend (onlyOwner)
+    function mintObservation(address to, string memory tokenURI) external {
+        require(msg.sender == owner, "Only owner can mint");
+        uint256 tokenId = ++_tokenIdCounter;
+        _tokenURIs[tokenId] = tokenURI;
+        _ownedTokens[to].push(tokenId);
+        emit ObservationMinted(to, tokenId, tokenURI);
+    }
+
+    //  ObservationMinted event for frontend notification (already emitted in mintObservation)
+
+    
 }
