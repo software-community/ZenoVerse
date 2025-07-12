cebc8e72f0fb5c30de061449ef754052a85eca36
Ratnadeep Paul <xfactor1289@gmail.com>
2025-07-12 21:47:42 +0530

    getObservationMetadata function tests done

--- test/ZenoVerse.js
+++ test/ZenoVerse.js
@@ -2,16 +2,42 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("ZenoVerse", function () {
-    let ZenoVerse, zenoverse, owner, addr1, addr2;
+  let ZenoVerse, zenoverse, owner, addr1, addr2;
 
-    beforeEach(async () => {
+  beforeEach(async () => {
     [owner, addr1, addr2] = await ethers.getSigners();
     ZenoVerse = await ethers.getContractFactory("ZenoVerse");
     zenoverse = await ZenoVerse.deploy();
-    });
+  });
 
-    it("should deploy and set the owner", async function () {
-        expect(await zenoverse.owner()).to.equal(owner.address);
-    });
-    
+  
+  it("should deploy and set the owner", async function () {
+    expect(await zenoverse.owner()).to.equal(owner.address);
+  });
+
+
+  it("should get the correct token metadata", async function () {
+    // Mint a token first
+    const uri = "https://api.zenoverse.com/token/0";
+    const tx = await zenoverse.mintObservation(owner.address, uri);
+    const receipt = await tx.wait();
+    expect(receipt.logs).to.not.be.empty;
+    const observationEvent = receipt.logs.find(
+      (log) => log.fragment && log.fragment.name === "ObservationMinted"
+    );
+    expect(observationEvent).to.not.be.undefined;
+    const tokenId = observationEvent.args.tokenId;
+    // Now check getObservationMetadata
+    const metadata = await zenoverse.getObservationMetadata(tokenId);
+    expect(metadata).to.equal(uri);
+  });
+
+
+  it("should revert the get metadata call if tokenId does not exist", async function () {
+    const tokenId = 0;
+    await expect(zenoverse.getObservationMetadata(tokenId)).to.be.revertedWith(
+      "ZenoVerse: Metadata query for nonexistent token"
+    );
+  });
+
 });
